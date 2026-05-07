688a21d4984c0e501be0ecdee237e217c9331290
RiyazPrynAI <riyaz.shaik@prynai.com>
2026-05-07 06:46:30 +0530

    defined state of the graph

---
+++ graph/state.py
@@ -0,0 +1,19 @@
+from typing import TypedDict, List
+
+
+class StateGraph(TypedDict):
+    """State that contains attriutes with defined types that flows through out state of the graph
+
+    Attributes:
+        question:str:question:input prompt to user
+        generation:str:LLM generation : response to the user
+        web_search:bool:dection to call websearch or not
+        document:list of document that generate from retrieval & websearch
+
+
+    """
+
+    question: str
+    generation: str
+    web_search: bool
+    documents: List[str]
